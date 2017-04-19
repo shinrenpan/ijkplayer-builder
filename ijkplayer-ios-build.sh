83e7dd37acb0ffa38447f233693a63fc14bfbd01
Shinren Pan <shinren.pan@gmail.com>
2017-04-19 14:27:57 +0800

    Update ijkplayer-ios-build.sh
    Space 替代 Tab

--- ijkplayer-ios-build.sh
+++ ijkplayer-ios-build.sh
@@ -6,24 +6,21 @@ VERSION="k0.7.6"
 
 do_clone_and_checkout()
 {
-	if [ -d ${CLONE_PATH} ]; then
-	  rm -rf ${CLONE_PATH}
+    if [ -d ${CLONE_PATH} ]; then
+        rm -rf ${CLONE_PATH}
     fi
 
-	git clone "https://github.com/Bilibili/ijkplayer.git" ${CLONE_PATH}
-	cd ${CLONE_PATH}
-	git checkout -B latest ${VERSION}
+    git clone "https://github.com/Bilibili/ijkplayer.git" ${CLONE_PATH}
+    cd ${CLONE_PATH}
+    git checkout -B latest ${VERSION}
 }
 
 do_run_ijkplayer_script()
 {
-	cd ${CLONE_PATH}
-
-	./init-ios.sh
-
-	cd ios
-
-	./compile-ffmpeg.sh clean
+    cd ${CLONE_PATH}
+    ./init-ios.sh
+    cd ios
+    ./compile-ffmpeg.sh clean
     ./compile-ffmpeg.sh all
 }
 
@@ -31,33 +28,32 @@ do_run_ijkplayer_script()
 # 編譯實機 framework
 do_build_device_framework()
 {
-	cd IJKMediaPlayer
-
-	xcodebuild -project "IJKMediaPlayer.xcodeproj" -target "${FRAMEWORK_NAME}" -configuration Release -arch arm64 -arch armv7 only_active_arch=no defines_module=yes -sdk iphoneos -OTHER_CFLAGS="-fembed-bitcode"
+    cd IJKMediaPlayer
+    xcodebuild -project "IJKMediaPlayer.xcodeproj" -target "${FRAMEWORK_NAME}" -configuration Release -arch arm64 -arch armv7 only_active_arch=no defines_module=yes -sdk iphoneos -OTHER_CFLAGS="-fembed-bitcode"
 
     if [ -d "${HOME}/Desktop/${FRAMEWORK_NAME}.framework" ]; then
-	  rm -rf "${HOME}/Desktop/${FRAMEWORK_NAME}.framework"
+        rm -rf "${HOME}/Desktop/${FRAMEWORK_NAME}.framework"
     fi
 
-	cp -r "build/Release-iphoneos/${FRAMEWORK_NAME}.framework" "${HOME}/Desktop/${FRAMEWORK_NAME}.framework"
+    cp -r "build/Release-iphoneos/${FRAMEWORK_NAME}.framework" "${HOME}/Desktop/${FRAMEWORK_NAME}.framework"
 }
 
 # 編譯模擬器 framework
 do_build_simulator_framework()
 {
-	xcodebuild -project "IJKMediaPlayer.xcodeproj" -target "${FRAMEWORK_NAME}" -configuration Release -arch i386 -arch x86_64 only_active_arch=no VALID_ARCHS="i386 x86_64" -sdk iphonesimulator -OTHER_CFLAGS="-fembed-bitcode"
+    xcodebuild -project "IJKMediaPlayer.xcodeproj" -target "${FRAMEWORK_NAME}" -configuration Release -arch i386 -arch x86_64 only_active_arch=no VALID_ARCHS="i386 x86_64" -sdk iphonesimulator -OTHER_CFLAGS="-fembed-bitcode"
 }
 
 # 利用 lipo 組成 fat framewok 並放到桌面
 do_lipo_framework()
 {
-	lipo -create -output "${HOME}/Desktop/${FRAMEWORK_NAME}.framework/${FRAMEWORK_NAME}" "build/Release-iphoneos/${FRAMEWORK_NAME}.framework/${FRAMEWORK_NAME}" "build/Release-iphonesimulator/${FRAMEWORK_NAME}.framework/${FRAMEWORK_NAME}"
+    lipo -create -output "${HOME}/Desktop/${FRAMEWORK_NAME}.framework/${FRAMEWORK_NAME}" "build/Release-iphoneos/${FRAMEWORK_NAME}.framework/${FRAMEWORK_NAME}" "build/Release-iphonesimulator/${FRAMEWORK_NAME}.framework/${FRAMEWORK_NAME}"
 
-	# 修改版本號
-	/usr/libexec/PlistBuddy -c "Set CFBundleShortVersionString ${VERSION}" "${HOME}/Desktop/${FRAMEWORK_NAME}.framework/Info.plist"
+    # 修改版本號
+    /usr/libexec/PlistBuddy -c "Set CFBundleShortVersionString ${VERSION}" "${HOME}/Desktop/${FRAMEWORK_NAME}.framework/Info.plist"
 
-	if [ -d ${CLONE_PATH} ]; then
-	  rm -rf ${CLONE_PATH}
+    if [ -d ${CLONE_PATH} ]; then
+        rm -rf ${CLONE_PATH}
     fi
 }
 
